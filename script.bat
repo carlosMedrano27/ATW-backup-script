fb81ecfac6268ec40658e2afb83e2fa794b06068
cmedrano <cmedrano@applaudostudios.com>
2019-10-27 11:34:03 -0600

    :tada: Script para backup

---
+++ script.bat
@@ -0,0 +1,18 @@
+@echo off
+
+for /f "tokens=1-3 delims=/- " %%a in ('date /t') do set FECHA=%%c%%b%%a
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set HORA=%%a%%b
+
+set BACKUP_FILE=Contenido_%FECHA%%HORA%.zip
+set BACKUP_LOG_FILE=logs/Contenido_%FECHA%.log
+
+echo. >> %BACKUP_LOG_FILE%
+echo Inicia respaldo >> %BACKUP_LOG_FILE%
+echo %FECHA% %HORA% >> %BACKUP_LOG_FILE%
+
+7zip.exe a -tzip "C:\Users\karlitosneto\Documents\backup\atw\" "C:\Users\karlitosneto\Documents\ATW-Ecommerce" >> %BACKUP_LOG_FILE%
+
+echo. >> %BACKUP_LOG_FILE%
+echo Fin respaldo >> %BACKUP_LOG_FILE%
+echo %FECHA% %HORA% >> %BACKUP_LOG_FILE%
+echo Log file: %BACKUP_LOG_FILE%
